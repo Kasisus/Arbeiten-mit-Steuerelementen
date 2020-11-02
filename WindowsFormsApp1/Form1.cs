082ea90a7d98a21b01b79cac8969a644e9f94832
Kasisus <73264146+Kasisus@users.noreply.github.com>
2020-11-02 20:07:36 +0100

    dsf

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,40 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CmdTest.Location = new Point(
+            CmdTest.Location.X + 20, CmdTest.Location.Y);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            CmdTest.Location = new Point(100, 200);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CmdTest.Size = new Size(
+            CmdTest.Size.Width + 20, CmdTest.Size.Height);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CmdTest.Size = new Size(50, 100);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LblAnzeige.Text = "Position: X: " + CmdTest.Location.X +
+", Y: " + CmdTest.Location.Y + "\n" + "Größe: Breite: " +
+          CmdTest.Size.Width + ", Höhe: " + CmdTest.Size.Height;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.Yellow;
+            LblAnzeige.BackColor = Color.FromArgb(192, 255, 0);
+        }
     }
 }
